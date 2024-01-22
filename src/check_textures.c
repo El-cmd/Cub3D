c227b408b58c0ad9312d744c0c647c8105cdfb7b
Eldoctor <lothvalentin@gmail.com>
2024-01-22 09:55:15 +0100

    add textures

--- src/check_textures.c
+++ src/check_textures.c
@@ -6,25 +6,25 @@
 /*   By: eldoctor <eldoctor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 13:17:49 by schai             #+#    #+#             */
-/*   Updated: 2024/01/19 14:52:03 by eldoctor         ###   ########.fr       */
+/*   Updated: 2024/01/22 09:53:22 by eldoctor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-//static int check_rgb(int *rgb)
-//{
-//    int i;
-//
-//    i = 0;
-//    while (i < 3)
-//    {
-//        if (rgb[i] < 0 || rgb[i] > 255)
-//            return (error_msg(NULL, INV_RGB, FAILURE));
-//        i++;
-//    }
-//    return (SUCCESS);
-//}
+static int check_rgb(int *rgb)
+{
+    int i;
+
+    i = 0;
+    while (i < 3)
+    {
+        if (rgb[i] < 0 || rgb[i] > 255)
+            return (error_msg(NULL, INV_RGB, FAILURE));
+        i++;
+    }
+    return (SUCCESS);
+}
 
 
 static int convert_rgb_to_hex(int *rgb)
@@ -43,18 +43,18 @@ static int convert_rgb_to_hex(int *rgb)
 
 int check_textures(t_texturedata *textures)
 {
-	//if (!textures->north || !textures->south || !textures->west
-	//	|| !textures->east)
-	//	return (error_msg(NULL, MISS_TEXTURE, FAILURE));
+	if (!textures->north || !textures->south || !textures->west
+		|| !textures->east)
+		return (error_msg(NULL, MISS_TEXTURE, FAILURE));
 	if (!textures->floor || !textures->ceiling)
 		return (error_msg(NULL, MISS_COLOR, FAILURE));
-	//if (check_file(textures->north, false) == FAILURE
-	//	|| check_file(textures->south, false) == FAILURE
-	//	|| check_file(textures->west, false) == FAILURE
-	//	|| check_file(textures->east, false) == FAILURE
-	//	|| check_rgb(textures->floor) == FAILURE
-	//	|| check_rgb(textures->ceiling) == FAILURE)
-	//	return (FAILURE);
+	if (check_file(textures->north, false) == FAILURE
+		|| check_file(textures->south, false) == FAILURE
+		|| check_file(textures->west, false) == FAILURE
+		|| check_file(textures->east, false) == FAILURE
+		|| check_rgb(textures->floor) == FAILURE
+		|| check_rgb(textures->ceiling) == FAILURE)
+		return (FAILURE);
     textures->hex_ceiling = convert_rgb_to_hex(textures->ceiling);
     textures->hex_floor =  convert_rgb_to_hex(textures->floor);
     return (SUCCESS);
